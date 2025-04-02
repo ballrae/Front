48889292ebb31260f98ea8ed9f18d7e91a436c95
Jihee <115629285+Anjihee@users.noreply.github.com>
2025-04-02 13:47:58 +0900

    feat. 애니메이션 환경 세팅

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,18 +1,34 @@
-import React from "react";
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useRef } from 'react';
+import { View, Text, StyleSheet, Pressable } from 'react-native';
+import LottieView from 'lottie-react-native';
 
-const HomeScreen = () =>{
-    return (
-        <View style={styles.container}>
-        <Text style={styles.text}>여기는 홈 화면입니다!</Text>
-      </View>
-    );
+const HomeScreen = () => {
+  const fireworkRef = useRef<LottieView>(null);
+
+  const handlePress = () => {
+    // 재시작
+    // 화면 여러번 누르면 애니메이션이 안 나오는 이슈 해결
+    fireworkRef.current?.reset();
+    fireworkRef.current?.play();
+  };
+
+  return (
+    <Pressable style={styles.container} onPress={handlePress}>
+      <Text style={styles.text}>홈 화면입니다!</Text>
+      <LottieView
+        ref={fireworkRef}
+        source={require('../assets/firework.json')}
+        autoPlay={false}
+        loop={false}
+        style={StyleSheet.absoluteFillObject} // 👈 화면 전체 꽉 채움!
+        resizeMode="cover" // 👈 화면 비율에 맞춰 확대
+      />
+    </Pressable>
+  );
 };
 
 export default HomeScreen;
 
-
-//일단 스타일시트
 const styles = StyleSheet.create({
   container: {
     flex: 1, justifyContent: 'center', alignItems: 'center',
